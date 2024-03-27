95f23ad80e7811f3c67df85324a1d9f20774c67a
sushil d. shrestha <61751094+sushildshrestha@users.noreply.github.com>
2024-03-27 14:32:35 +0545

    get playlist and current cong index

--- lib/presentation/screen/song_page.dart
+++ lib/presentation/screen/song_page.dart
@@ -18,129 +18,142 @@ class SongPage extends StatefulWidget {
 class _SongPageState extends State<SongPage> {
   @override
   Widget build(BuildContext context) {
-    return Consumer<PlaylistProvider>(
-        builder: (context, value, child) => Scaffold(
-              backgroundColor: Theme.of(context).colorScheme.background,
-              body: SafeArea(
-                  child: Column(
+    return Consumer<PlaylistProvider>(builder: (context, value, child) {
+//get the playlist
+      final playList = value.playList;
+//get current song index
+      final currentSong = playList[value.currentSongIndex ?? 0];
+
+      //return Scafford
+      return Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: SafeArea(
+            child: Column(
+          children: [
+            //appBar
+            Padding(
+              padding: const EdgeInsets.only(left: 12.0, right: 12),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  //appBar
-                  Padding(
-                    padding: const EdgeInsets.only(left: 12.0, right: 12),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        IconButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            icon: const Icon(Icons.arrow_back)),
-                        //title
-                        Text(
-                          centerTitleText,
-                          style: const TextStyle(fontSize: 18),
-                        ),
-
-                        //menubutton
-                        IconButton(
-                            onPressed: () {}, icon: const Icon(Icons.menu))
-                      ],
-                    ),
+                  IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(Icons.arrow_back)),
+                  //title
+                  Text(
+                    centerTitleText,
+                    style: const TextStyle(fontSize: 18),
                   ),
 
-                  //album artwork
-                  NeuBox(
-                      child: Column(
-                    children: [
-                      //image
-                      ClipRRect(
-                          borderRadius: BorderRadius.circular(12),
-                          child: Image.asset('assets/images/jheli.jpg')),
-
-                      //songName and artistName, icon
-
-                      Padding(
-                        padding: const EdgeInsets.only(top: 8.0, right: 8),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            SizedBox(),
-                            //songName
-                            Column(
-                              //crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  songName,
-                                  style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                Text(
-                                  artistName,
-                                  style: TextStyle(color: Colors.grey[600]),
-                                )
-                              ],
-                            ),
-
-                            Icon(Icons.favorite_border_outlined)
-                          ],
-                        ),
-                      )
-                    ],
-                  )),
-                  SizedBox(
-                    height: 25,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      //start time
-                      Text(initialTime),
-
-                      //suffle icon
-                      IconButton(onPressed: () {}, icon: Icon(Icons.shuffle)),
-
-                      //repeat icon
-                      IconButton(onPressed: () {}, icon: Icon(Icons.repeat)),
-
-                      //end time
-                      Text(endTime)
-                    ],
-                  ),
-                  SizedBox(
-                    height: 25,
-                  ),
-                  Slider(
-                      min: 0,
-                      max: 100,
-                      value: 50,
-                      activeColor: Colors.green,
-                      onChanged: (value) {}),
-                  SizedBox(
-                    height: 25,
-                  ),
-
-                  Padding(
-                    padding: const EdgeInsets.only(left: 12.0, right: 12),
-                    child: Row(
-                      children: [
-                        Expanded(
-                            child: NeuBox(child: Icon(Icons.skip_previous))),
-                        const SizedBox(
-                          width: 15,
-                        ),
-                        Expanded(
-                            flex: 2, child: NeuBox(child: Icon(Icons.pause))),
-                        const SizedBox(
-                          width: 15,
-                        ),
-                        Expanded(child: NeuBox(child: Icon(Icons.skip_next))),
-                      ],
-                    ),
-                  )
+                  //menubutton
+                  IconButton(onPressed: () {}, icon: const Icon(Icons.menu))
                 ],
-              )),
-            ));
+              ),
+            ),
+
+            //album artwork
+            NeuBox(
+                child: Column(
+              children: [
+                //image
+                ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Image.asset(currentSong.albumImagePath)),
+
+                //songName and artistName, icon
+
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0, right: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(),
+                      //songName
+                      Column(
+                        //crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            currentSong.songName,
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            currentSong.artistName,
+                            style: TextStyle(color: Colors.grey[600]),
+                          )
+                        ],
+                      ),
+
+                      Icon(Icons.favorite_border_outlined)
+                    ],
+                  ),
+                )
+              ],
+            )),
+            SizedBox(
+              height: 25,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                //start time
+                Text(initialTime),
+
+                //suffle icon
+                IconButton(onPressed: () {}, icon: Icon(Icons.shuffle)),
+
+                //repeat icon
+                IconButton(onPressed: () {}, icon: Icon(Icons.repeat)),
+
+                //end time
+                Text(endTime)
+              ],
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Slider(
+                min: 0,
+                max: 100,
+                value: 0,
+                activeColor: Colors.green,
+                onChanged: (value) {}),
+            SizedBox(
+              height: 25,
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(left: 12.0, right: 12),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: GestureDetector(
+                          onTap: () {},
+                          child: NeuBox(child: Icon(Icons.skip_previous)))),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  Expanded(
+                      flex: 2,
+                      child: GestureDetector(
+                          onTap: () {},
+                          child: NeuBox(child: Icon(Icons.pause)))),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  Expanded(
+                      child: GestureDetector(
+                          onTap: () {},
+                          child: NeuBox(child: Icon(Icons.skip_next)))),
+                ],
+              ),
+            )
+          ],
+        )),
+      );
+    });
   }
 }
